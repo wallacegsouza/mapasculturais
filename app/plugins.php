8cb6e4c274e577012348c7bd616ed9912365ee5b
Wallace G. Souza <wallacegomesdesouza@gmail.com>
2023-05-24 16:25:07 -0300

    [wip] adicionado login

---
+++ app/plugins.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'plugins' => [
+        'EvaluationMethodTechnical' => ['namespace' => 'EvaluationMethodTechnical', 'config' => ['step' => 0.1]],
+        'EvaluationMethodSimple' => ['namespace' => 'EvaluationMethodSimple'],
+        'EvaluationMethodDocumentary' => ['namespace' => 'EvaluationMethodDocumentary'],
+        'MultipleLocalAuth' => ['namespace' => 'MultipleLocalAuth'],
+    ]
+];
